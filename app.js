626f3076a9a965a518a01e205faca1ab4eef6310
GotesDr <102544885+GotesDr@users.noreply.github.com>
2023-02-12 11:17:54 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ function returnColor() {
     document.body.style.background = `rgb(255, 255, 255)`;
 
 }
+function buttonColor() {
+    let r = randomNumber(0, 255);
+    let g = randomNumber(0, 255);
+    let b = randomNumber(0, 255);
+
+    document.getElementById('a').style.background = `rgb(${r}, ${g}, ${b})`;
+}
 
 function randomNumber(min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
